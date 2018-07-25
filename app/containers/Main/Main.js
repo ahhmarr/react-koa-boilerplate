3f4d5fde31cb1122209dbc794ef9931c8f2cc38b
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2018-07-25 17:45:39 +0530

    added sass

--- app/containers/Main/Main.js
+++ app/containers/Main/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./Main.scss";
 const Main = props => {
-  return <div>{props.children}</div>;
+  return <div className="main">{props.children}</div>;
 };
 
 export default Main;
